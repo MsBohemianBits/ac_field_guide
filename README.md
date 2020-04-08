1e0ec1edca6f4aba09aed651b44ed567f2d4c60d
Tia <tia@techiasolutions.com>
2020-04-07 20:38:29 -0500

    Added sliver appbar/list and updated main app bar

--- README.md
+++ README.md
@@ -4,13 +4,6 @@ AC Field Guide
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+Work in progress for Animal Crossing app brought to you by 100DaysOfCode. Stay tuned! Have input? Go ahead and add a [feature request](https://github.com/MsBohemianBits/ac_field_guide/issues/new?assignees=&labels=&template=feature_request.md&title=).
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+### Estimated month of completion: July
--- lib/main.dart
+++ lib/main.dart
@@ -24,24 +24,65 @@ class IndexPage extends StatefulWidget {
 }
 
 class _IndexPageState extends State<IndexPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add_alert),
+            tooltip: 'Add reminder',
+            onPressed: () {
+              /** **/
+            },
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset('assets/images/Animal_Crossing_Logo.png')
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Add stuff',
-        child: Icon(Icons.add),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 150.0,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              title: Text('Upcoming events'),
+              background: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Image.network(
+                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                  const DecoratedBox(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment(0.0, 0.5),
+                        end: Alignment(0.0, 0.0),
+                        colors: <Color>[
+                          Color(0x60000000),
+                          Color(0x00000000),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SliverFixedExtentList(
+            itemExtent: 50.0,
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  alignment: Alignment.center,
+                  color: Colors.purpleAccent[100 * (index % 9)],
+                  child: Text('List Item $index'),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
